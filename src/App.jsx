c855b5603f9c957bd8b4d896e6738f900e0237bb
Soham Kulkarni <soham@vectorconsulting.in>
2025-05-18 22:32:35 +0530

    removed import command

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import axios from "axios";
-import { Inspector } from 'react-inspector';
 
 function App() {
   const [data, setData] = useState()
