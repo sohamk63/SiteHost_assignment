eff5b8bff0240a741b3c1fa2f86b8b32c4dd4699
Soham Kulkarni <soham@vectorconsulting.in>
2025-05-18 22:01:04 +0530

    adding this project

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+import axios from "axios";
+import { Inspector } from 'react-inspector';
+
+function App() {
+  const [data, setData] = useState()
+
+  const getDomainData = () => {
+    axios.get("http://127.0.0.1:8000/dns-data")
+    .then((response) => {
+      setData(response.data);
+    })
+    .catch((error) =>{
+      console.error("Something Went Wrong: ", error);
+    })
+  }
+  
+  return (
+    <>
+
+      <div style={{
+        marginBottom : '5px'
+      }}>
+        <h2>DNS Data</h2>
+        <div >
+
+        <pre
+          style={{
+            textAlign: 'left',
+            // padding: '20px',
+            margin: '20px auto',
+            maxWidth: '800px',
+            backgroundColor: '#191970',
+            borderRadius: '8px',
+            overflowX: 'auto',
+            overflowX: 'auto',
+            maxHeight:'400px',
+            fontFamily: 'monospace',
+            fontSize: '14px',
+            lineHeight: '1.5',
+            whiteSpace: 'pre-wrap',  
+            wordBreak: 'break-word' 
+          }}
+        >
+        {JSON.stringify(data, null, 2)}
+      </pre>
+
+      
+      </div>
+      </div>
+      <button 
+      onClick={getDomainData}
+      >
+        Get Domain Info
+      </button>    
+    </>
+  )
+}
+
+export default App
