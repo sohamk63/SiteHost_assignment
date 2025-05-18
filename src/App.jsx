cc3e324ef73c21fad1f6937b8fb2db66887c5787
Soham Kulkarni <soham@vectorconsulting.in>
2025-05-18 23:48:58 +0530

    added logs

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [data, setData] = useState()
 
   const getDomainData = () => {
+    console.log('API URL:', process.env.REACT_APP_API_BASE_URL);
     axios.get(`${process.env.SH_BACKEND_URL}/dns-data`)
     .then((response) => {
       setData(response.data);
