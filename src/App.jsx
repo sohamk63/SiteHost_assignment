e4c81ea8b83b2ffa5e9d57dd09a10a5dc884d609
Soham Kulkarni <soham@vectorconsulting.in>
2025-05-18 23:53:59 +0530

    changeg env variable name

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState()
 
-  const getDomainData = () => {
-    console.log('API URL:', process.env.SH_BACKEND_URL);
-    axios.get(`${process.env.SH_BACKEND_URL}/dns-data`)
+  const getDomainData = () => { 
+    axios.get(`${process.env.REACT_APP_SH_BACKEND_URL}/dns-data`)
     .then((response) => {
       setData(response.data);
     })
